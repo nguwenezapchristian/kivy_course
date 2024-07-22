60ee8dd6f6b41de04e148d10d25c6de4b4ed8632
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-22 22:10:33 +0200

    created different shapes inside python file.

--- the_lab_canvas/main.py
+++ the_lab_canvas/main.py
@@ -1,5 +1,19 @@
 from kivy.app import App
+from kivy.graphics.vertex_instructions import Line, Rectangle
+from kivy.graphics.context_instructions import Color
+from kivy.uix.widget import Widget
 
+
+class ExampleCanvas4(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Color(1, 1, 0)
+            Line(points=(100, 50, 100, 150), width=2)
+            Color(0, 1, 0)
+            Line(ellipse=(50, 150, 100, 50), width=2)
+            Color(0,0,1)
+            Rectangle(pos=(50, 0), size=(100, 50))
 
 class TheLabCanvasApp(App):
     pass
