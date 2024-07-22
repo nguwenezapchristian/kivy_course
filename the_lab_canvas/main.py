bd0b3b11dfa1a300b3ce0072571e0ed2547a3379
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-22 23:07:58 +0200

    change the pos of an object by clicking the Btn

--- the_lab_canvas/main.py
+++ the_lab_canvas/main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.graphics.vertex_instructions import Line, Rectangle
 from kivy.graphics.context_instructions import Color
 from kivy.uix.widget import Widget
+from kivy.metrics import dp
 
 
 class ExampleCanvas4(Widget):
@@ -13,7 +14,12 @@ class ExampleCanvas4(Widget):
             Color(0, 1, 0)
             Line(ellipse=(50, 150, 100, 50), width=2)
             Color(0,0,1)
-            Rectangle(pos=(50, 0), size=(100, 50))
+            self.rect = Rectangle(pos=(50, 0), size=(100, 50))
+    
+    def on_click_action(self):
+        x, y = self.rect.pos
+        x += dp(10)
+        self.rect.pos = (x, y)
 
 class TheLabCanvasApp(App):
     pass
