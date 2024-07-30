7a9972c642fa434134040f3e898642dddcb7a1b0
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-30 20:17:34 +0200

    Perspective transformation

--- The Galaxy/main.py
+++ The Galaxy/main.py
@@ -58,8 +58,8 @@ class MainWidget(Widget):
                 offset += 1
     
     def transform(self, x, y):
-        return self.transform_2D(x, y)
-        # return self.transform_perspective(x, y)
+        # return self.transform_2D(x, y)
+        return self.transform_perspective(x, y)
     
     def transform_2D(self, x, y):
         return int(x), int(y)
